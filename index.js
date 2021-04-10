fcde0d40f33b7842143012522d30adc1a22834e0
tak59 <tak59@live.com>
2021-04-10 06:12:00 -0400

    commit tree f56f5c006c39f3efb784a8a3ae150c00a8d5f45e

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+const driver = { name: "Bob"}
+function updateDriverWithKeyAndValue(driver, key, value) {
+    const updatedriver = { ...driver };
+
+    updatedriver[key] = value;
+  
+    return updatedriver;
+}
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+    driver[key] = value;
+
+    return driver;
+}
+
+function deleteFromDriverByKey(driver, key) {
+    const newdriver = {...driver}
+    delete newdriver[key];
+    return newdriver;
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key) {
+    delete driver[key];
+    return driver;
+}
